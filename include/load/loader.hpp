53b8170c6a6511862108d3a436143b3e9679d1d2
xunzhang <xunzhangthu@gmail.com>
2013-11-17 12:17:10 +0800

    expand added, first version of loader finished, to be tested.

--- include/load/loader.hpp
+++ include/load/loader.hpp
@@ -16,19 +16,54 @@
 #define FILE_71d45241_99cd_4d2c_cb1a_9d3e9ac6203c_HPP
 
 #include <functional>
+
 #include "paracel_types.hpp"
+#incude "load/scheduler.hpp"
 
 namespace paracel {
 
+typedef std::function< paracel::list_type<paracel::str_type>(paracel::str_type) parser_type;
+
 template <class T>
 class loader {
+
 public:
+  
+  loader(T fns, paracel::Comm comm) : filenames(fns), m_comm(comm) {};
+  
+  loader(T fns, paracel::Comm comm, parser_type f, paracel::str_type pt) : filenames(fns), m_comm(comm) parserfunc(f), pattern(pt){};
+  
+  loader(T fns, paracel::Comm comm, parser_type f,  paracel::str_type pt, bool flag) : filenames(fns), m_comm(comm) parserfunc(f), pattern(pt), mix(flag) {};
+  
+  void graph_load(paracel::dict_type<size_t, paracel::str_type> & rm, 
+  		paracel::dict_type<size_t, paracel::str_type> & cm,
+      		paracel::dict_type<size_t, int> & dm,
+      		paracel::dict_type<size_t, int> & col_dm) {
+    
+    paracel::scheduler scheduler(m_comm, pattern, mix);
+    auto fname_lst = paracel::expand(filenames);
+    auto loads = paracel::files_partition(fname_lst, m_comm.get_size());
+    auto linelst = scheduler.schedule_load(loads);
+    auto result = scheduler.lines_organize(linelst, parserfunc);
+    auto stf = scheduler.exchange(result);
+    paracel::list_type<std::tuple<size_t, size_t, double> > stf_new;
+    scheduler.index_mapping(stf, stf_new, rm, cm, dm, col_dm);
+  }
+
+  void load(paracel::dict_type<size_t, paracel::str_type> & rm,
+  	paracel::dict_type<size_t, paracel::str_type> & cm) {
+
+    paracel::dict_type<size_t, int> dm;
+    paracel::dict_type<size_t, int> col_dm;
+    graph_load(blk_mtx, rm, cm, dm, col_dm);
+  }
+
 private:
   T filenames;
-  Comm m_comm;
-  std::function< paracel::list_type<paracel::str_type>(paracel::str_type) parser;
+  paracel::Comm m_comm;
   paracel::str_type pattern = "fmap";
   bool mix = false;
+  std::function< paracel::list_type<paracel::str_type>(paracel::str_type) parserfunc;
 };
 
 } // namespace paracel
--- include/utils/ext_utility.hpp
+++ include/utils/ext_utility.hpp
@@ -15,6 +15,8 @@
 #ifndef FILE_56636034_fc34_4f9e_4343_4111ca3b127d_HPP
 #define FILE_56636034_fc34_4f9e_4343_4111ca3b127d_HPP
 
+#include <sys/stat.h>
+#include <glob.h>
 #include <map>
 #include "paracel_types.hpp"
 
@@ -48,6 +50,10 @@ slst_type str_split(const paracel::str_type & str, const paracel::str_type & sep
   return result;
 }
 
+bool endswith(const paracel::str_type & str, const std::string & c) {
+  return str.rfind(c) == (str.length() - c.length());
+}
+
 // ['a', 'c', 'b', 'a', 'a', 'b'] -> [3, 2, 1]
 paracel::list_type<int> sort_and_cnt(const paracel::list_type<paracel::str_type> & in) {
   paracel::list_type<int> r;
@@ -63,6 +69,81 @@ paracel::list_type<int> sort_and_cnt(const paracel::list_type<paracel::str_type>
     r.push_back(mp.second);
   }
   return r;
+}
+
+bool isfile(paracel::str_type & f) {
+  struct stat st;
+  stat(f, &st);
+  if(S_ISREG(st.st_mode)) {
+    return true;
+  }
+  return false;
+}
+
+bool isdir(paracel::str_type & d) {
+  struct stat st;
+  stat(d, &st);
+  if(S_ISDIR(st.st_mode)) {
+    return true;
+  }
+  return false;
+}
+
+paracel::list_type<paracel::str_type> paracel_glob(const paracel::string & pattern) {
+  glob_t glob_res;
+  paracel::list_type<paracel::str_type> lst;
+  glob(pattern.c_str(), GLOB_TILDE, NULL, &glob_res);
+  for(size_t i = 0; i < glob_res.gl_pathc, ++i) {
+    lst.push_back(std::string(glob_res.gl_pathv[i]));
+  }
+  globfree(&glob_result);
+  return lst;
+}
+
+// expand a dictory name recursively
+paracel::list_type<paracel::str_type> expand_dir_rec(const paracel::str_type & dname) {
+ paracel::list_type<paracel::str_type> fl;
+ if(!endswith(dname, "/")) {
+   dname = dnmae + "/";
+ }
+ auto lst = paracel_glob(dname + "*");
+ for(auto & name : lst) {
+   if(isfile(name)) {
+     fl.push_back(name);
+   } else {
+     auto tmp_lst = expand_dir_rec(name);
+     fl.insert(fl.end(); tmp_lst.begin(), tmp_lst.end());
+   }
+ }
+ return fl;
+}
+
+// fname can be 'demo.txt' or 'demo_dir'
+paracel::list_type<paracel::str_type> expand(const paracel::str_type & fname) {
+  paracel::list_type<paracel::str_type> fl;
+  if(isfile(fname)) {
+    fl.push_back(fname);
+    return fl;
+  } else if(isdir(fname)) {
+    return expand_dir_rec(fname); 
+  } else {
+    // expect a reg exp: '/home/xunzhang/*.csv'
+    return paracel_glob(fname);
+  } 
+}
+
+// fname_lst can be ['demo.txt', 'demo_dir', ...]
+paracel::list_type<paracel::str_type> expand(const paracel::list_type<paracel::str_type> & fname_lst) {
+  paracel::list_type<paracel::str_type> fl;
+  for(auto & name : lst) {
+    if(isfile(name)) {
+      fl.push_back(name);
+    } else {
+      auto tmp_lst = expand_dir_rec(name);
+      fl.insert(fl.end(); tmp_lst.begin(), tmp_lst.end());
+    }
+  }
+  return lst;
 }
 
 } // namespace paracel
