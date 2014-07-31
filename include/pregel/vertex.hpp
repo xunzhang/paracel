db9f94c1f733af43eed581598fd3d1e30790a039
xunzhang <xunzhangthu@gmail.com>
2014-07-31 21:32:38 +0800

    first commit of pregel impl paradigm in paracel

---
+++ include/pregel/vertex.hpp
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 2014, Douban Inc. 
+ *   All rights reserved. 
+ *
+ * Distributed under the BSD License. Check out the LICENSE file for full text.
+ *
+ * Paracel - A distributed optimization framework with parameter server.
+ *
+ * Downloading
+ *   git clone http://code.dapps.douban.com/paracel.git
+ *
+ * Authors: Hong Wu <xunzhangthu@gmail.com>
+ *
+*/
+#ifndef FILE_ace68a02_0c2c_1d7c_0dd7_3f42027dec00_HPP
+#define FILE_ace68a02_0c2c_1d7c_0dd7_3f42027dec00_HPP
+
+
+namespace paracel {
+namespace pregel {
+
+} // namespace pregel
+} // namespace paracel
+
+#endif
