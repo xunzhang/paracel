75042f74c32928d94071beacc7532b933d21aa69
xunzhang <xunzhangthu@gmail.com>
2014-11-07 10:56:43 +0800

    DFS, BFS in graph.hpp

--- include/graph.hpp
+++ include/graph.hpp
@@ -17,6 +17,7 @@
 
 #include <tuple>
 #include <utility>
+#include <queue>
 #include <algorithm>
 #include <msgpack.hpp>
 #include "paracel_types.hpp"
@@ -103,120 +104,6 @@ private:
   size_t e_sz = 0;
   paracel::list_type<paracel::list_type<std::pair<size_t, double> > > adj;
 };
-
-/*
-// (size_t, size_t, double) type directed graph
-class bigraph {
-public:
-  
-  bigraph() {}
-
-  bigraph(size_t v_sz) {
-    this->v_sz = v_sz;
-    adj.resize(v_sz);
-    reverse_adj.resize(v_sz);
-  }
-  
-  bigraph(paracel::list_type<std::pair<size_t, size_t> > & edges) {
-    for(auto & edge : edges) {
-      if(edge.first > v_sz) v_sz = edge.first;
-      if(edge.second > v_sz) v_sz = edge.second;
-    }
-    v_sz += 1;
-    adj.resize(v_sz);
-    reverse_adj.resize(v_sz);
-    for(auto & edge : edges) {
-      add_edge(edge.first, edge.second);
-    }
-  }
-
-  void construct_from_triples(const paracel::list_type<std::tuple<size_t, size_t, double> > & tpls) {
-    for(auto & tpl : tpls) {
-      if(std::get<0>(tpl) > v_sz) v_sz = std::get<0>(tpl);
-      if(std::get<1>(tpl) > v_sz) v_sz = std::get<1>(tpl);
-    }
-    v_sz += 1;
-    adj.resize(v_sz);
-    reverse_adj.resize(v_sz);
-    for(auto & tpl : tpls) {
-      add_edge(std::get<0>(tpl), std::get<1>(tpl), std::get<2>(tpl));
-    }
-  }
-
-  bigraph(paracel::list_type<std::tuple<size_t, size_t, double> > & tpls) {
-    construct_from_triples(tpls);
-  }
-
-  void add_edge(size_t v, size_t w) {
-    adj[v].emplace_back(std::make_pair(w, 1.));
-    reverse_adj[w].emplace_back(std::make_pair(v, 1.));
-    e_sz += 1;
-  }
-
-  void add_edge(size_t v, size_t w, double wgt) {  
-    adj[v].emplace_back(std::make_pair(w, wgt));
-    reverse_adj[w].emplace_back(std::make_pair(v, wgt));
-    e_sz += 1;
-  }
-
-  template <class F>
-  void traverse(F & func) {  
-    for(int i = 0; i < v_sz; ++i) {
-      for(auto & e_w : adj[i]) {
-        func(std::make_tuple((size_t)i, e_w.first, e_w.second));
-      }
-    }
-  }
-
-  template <class F>
-  void traverse(size_t v, F & func) {
-    for(auto & e_w : adj[v]) {
-      func(std::make_tuple(v, e_w.first, e_w.second));
-    }
-  }
-  
-  void dump_triples(paracel::list_type<std::tuple<size_t, size_t, double> > & tpls) {
-    tpls.resize(0);
-    for(int i = 0; i < v_sz; ++i) {
-      for(auto & e_w : adj[i]) {
-        tpls.emplace_back(std::make_tuple((size_t)i, e_w.first, e_w.second));
-      }
-    }
-  }
-  
-  bigraph reverse() {
-    // TODO
-  }
-
-  inline size_t v() { return v_sz; }
-  
-  inline size_t e() { return e_sz; }
-  
-  paracel::list_type<std::pair<size_t, double> > adjacent(size_t v) { return adj[v]; }
-  
-  inline size_t outdegree(size_t v) { return adj[v].size(); }
-
-  inline size_t indegree(size_t v) {}
-  
-  inline double avg_degree() { return e_sz / v_sz; }
-  
-  inline int selfloops() {
-    int cnt = 0;
-    for(size_t v = 0; v < v_sz; ++v) {
-      for(auto & ww : adj[v]) {
-        if(v == std::get<0>(ww)) { cnt ++; }
-      }
-    }
-    return cnt / 2;
-  }
-
-private:
-  size_t v_sz = 0; 
-  size_t e_sz = 0;
-  paracel::list_type<paracel::list_type<std::pair<size_t, double> > > adj;
-  paracel::list_type<paracel::list_type<std::pair<size_t, double> > > reverse_adj;
-};
-*/
 
 template <class T = paracel::str_type>
 class bigraph {
@@ -379,6 +266,89 @@ public:
  public:
   MSGPACK_DEFINE(v_sz, e_sz, adj, reverse_adj);
 };
+
+template <class G, class T, class F>
+class DFS {
+ public:
+  DFS(G graph, T src, F lambda) {
+    dfs(graph, src, lambda);
+  }
+  inline bool visited(const T & v) { return marked.count(v); }
+ private:
+  void dfs(G & graph, const T & v, F & func) {
+    marked[v] = true;
+    func(v);
+    for(auto & kv : graph.adjacent(v)) {
+      auto w = kv.first;
+      if(!marked.count(w)) {
+        dfs(graph, w, func);
+      }
+    }
+  } // dfs
+ private:
+  paracel::dict_type<T, bool> marked;
+}; // class DFS
+
+// specialization DFS for undirected_graph
+template <class F>
+class DFS<paracel::undirected_graph, size_t, F> {
+ public:
+  DFS(paracel::undirected_graph graph, size_t src, F lambda) {
+    dfs(graph, src, lambda);
+  }
+  inline bool visited(size_t v) { return marked.count(v); }
+ private:
+  void dfs(paracel::undirected_graph graph, size_t v, F & func) {
+    marked[v] = true;
+    func(v);
+    for(auto & pr : graph.adjacent(v)) {
+      if(!marked.count(pr.first)) {
+        dfs(graph, pr.first, func);
+      }
+    }
+  } // dfs
+ private:
+  paracel::dict_type<size_t, bool> marked;
+}; // specialization class DFS
+
+template <class G, class T, class F>
+class BFS {
+ public:
+  BFS(G graph, T src, F func) {
+    bfs(graph, src, func);
+  }
+  inline bool visited(const T & v) { return marked.count(v); }
+  inline T edgeTo(const T & v) { return edge_to[v]; }
+  inline int dist(const T & v) { return dist_to[v]; }
+ private:
+  void bfs(G & grp, const T & v, F & func) {
+    std::queue<T> q;
+    q.push(v);
+    marked[v] = true;
+    func(v);
+    dist_to[v] = 0;
+    while(!q.empty()) {
+      T node = q.front();
+      q.pop();
+      bool has_unmarked = false;
+      for(auto & kv : grp.adjacent(node)) {
+        auto w = kv.first;
+        if(!marked.count(w)) {
+          has_unmarked = true;
+          q.push(w);
+          marked[w] = true;
+          func(w);
+          edge_to[w] = node;
+          dist_to[w] = dist_to[node] + 1;
+        }
+      }
+    }
+  }
+ private:
+  paracel::dict_type<T, bool> marked;
+  paracel::dict_type<T, T> edge_to;
+  paracel::dict_type<T, int> dist_to;
+}; // class BFS
 
 } // namespace paracel
 
